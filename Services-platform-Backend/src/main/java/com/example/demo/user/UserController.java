66126368326cb92ce434fa3230f3022d4abab87a
Faisal-Alsharif <ifaisal.alsharif@gmail.com>
2021-12-12 11:56:00 +0300

    create UserController & UserService & UserRepository and test Git, Post requests

--- Services-platform-Backend/src/main/java/com/example/demo/user/UserController.java
+++ Services-platform-Backend/src/main/java/com/example/demo/user/UserController.java
@@ -1,4 +1,33 @@
 package com.example.demo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "users")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return userService.getUsers();
+    }
+
+    @GetMapping("{id}")
+    public User getUser(@PathVariable String id){
+        return userService.getUser(id);
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user){
+        return userService.addUser(user);
+    }
 }
--- Services-platform-Backend/src/main/java/com/example/demo/user/UserService.java
+++ Services-platform-Backend/src/main/java/com/example/demo/user/UserService.java
@@ -1,4 +1,30 @@
 package com.example.demo.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        Integer user_id = Integer.parseInt(id);
+        return userRepository.findById(user_id).orElse(null);
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
 }
