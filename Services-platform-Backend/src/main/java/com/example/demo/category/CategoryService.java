d58039ece600260ea11206394c2b457191938c72
Faisal-Alsharif <ifaisal.alsharif@gmail.com>
2021-12-12 13:50:29 +0300

    create Category entity, CategoryRepo, CategoryService and CategoryController with Get, Post, Put Requests

--- Services-platform-Backend/src/main/java/com/example/demo/category/CategoryService.java
+++ Services-platform-Backend/src/main/java/com/example/demo/category/CategoryService.java
@@ -1,4 +1,39 @@
 package com.example.demo.category;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Autowired
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List getCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category getCategory(String id) {
+        Integer category_id = Integer.parseInt(id);
+        return categoryRepository.findById(category_id).orElse(null);
+    }
+
+    public Category addCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void updateCategory(String id, Category category) {
+        Integer category_id = Integer.parseInt(id);
+        Category category1 = categoryRepository.findById(category_id).orElse(null);
+        if(category1 != null){
+            category1.setCategory_name(category.getCategory_name());
+            categoryRepository.save(category1);
+        }
+    }
 }
--- Services-platform-Backend/src/main/java/com/example/demo/user/User.java
+++ Services-platform-Backend/src/main/java/com/example/demo/user/User.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int user_id;
     private String user_name;
     private String email;
