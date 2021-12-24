217ed4f9683e255c86c93d3fccfaa93823af6a67
Faisal-Alsharif <ifaisal.alsharif@gmail.com>
2021-12-24 16:54:01 +0300

    improve styling and make some changes in layout in Categories and UserHome component

--- services-platform-frontend/src/Components/categories/Categories.js
+++ services-platform-frontend/src/Components/categories/Categories.js
@@ -38,32 +38,26 @@ function Categories() {
             <h4>Home services</h4>
             <div className="service-icons">
               <div>
-                <img src="./images/plumbing.png" width="30px" height="30px" />
-                <label className="icon-txt">
-                  All plumbing repair and installation work.
-                </label>
+                <img src="./images/plumbing.png" width="25px" height="25px" />
+                <label className="icon-txt">plumbing repair work.</label>
               </div>
               <div>
-                <img src="./images/bolt.png" width="30px" height="30px" />
-                <label className="icon-txt">
-                  Electrical installation and repair work.
-                </label>
+                <img src="./images/bolt.png" width="25px" height="25px" />
+                <label className="icon-txt">Electrical repair work.</label>
               </div>
               <div>
                 <img
                   src="./images/air-conditioner.png"
-                  width="30px"
-                  height="30px"
+                  width="25px"
+                  height="25px"
                 />
                 <label className="icon-txt">
-                  Air conditioning repair, and maintenance.
+                  Air conditioning maintenance.
                 </label>
               </div>
               <div>
-                <img src="./images/gas.png" width="30px" height="30px" />
-                <label className="icon-txt">
-                  Delivery of fuel inside the city.
-                </label>
+                <img src="./images/gas.png" width="25px" height="25px" />
+                <label className="icon-txt">Delivery of fuel.</label>
               </div>
             </div>
           </div>
@@ -72,30 +66,22 @@ function Categories() {
           <img src="./images/car-repair.png" height="130" width="130"></img>
           <div className="category-name">
             <h4>Car services</h4>
-            <div className="service-icons">
+            <div className="service-icons car">
               <div>
-                <img src="./images/tire.png" width="30px" height="30px" />
-                <label className="icon-txt">
-                  Fixing all kinds of tire issues.
-                </label>
+                <img src="./images/tire.png" width="25px" height="25px" />
+                <label className="icon-txt">Fixing tire issues.</label>
               </div>
               <div>
-                <img src="./images/battery.png" width="30px" height="30px" />
-                <label className="icon-txt">
-                  Jump starting a battery or replacing it.
-                </label>
+                <img src="./images/battery.png" width="25px" height="25px" />
+                <label className="icon-txt">replacing a battery.</label>
               </div>
               <div>
-                <img src="./images/locked-car.png" width="30px" height="30px" />
-                <label className="icon-txt">
-                  Unlocking vehicles in case of emergencies.
-                </label>
+                <img src="./images/locked-car.png" width="25px" height="25px" />
+                <label className="icon-txt">Unlocking vehicles.</label>
               </div>
               <div>
-                <img src="./images/gas-pump.png" width="30px" height="30px" />
-                <label className="icon-txt">
-                  Delivery of fuel inside the city.
-                </label>
+                <img src="./images/gas-pump.png" width="25px" height="25px" />
+                <label className="icon-txt">Delivery of Gas.</label>
               </div>
             </div>
           </div>
@@ -106,11 +92,11 @@ function Categories() {
             <h4>Phone services</h4>
             <div className="service-icons">
               <div>
-                <img src="./images/apple.png" width="30px" height="30px" />
+                <img src="./images/apple.png" width="25px" height="25px" />
                 <label className="icon-txt">iPhone maintenance.</label>
               </div>
               <div>
-                <img src="./images/android.png" width="30px" height="30px" />
+                <img src="./images/android.png" width="25px" height="25px" />
                 <label className="icon-txt">Android maintenance.</label>
               </div>
             </div>
--- services-platform-frontend/src/Components/userHome/UserHome.js
+++ services-platform-frontend/src/Components/userHome/UserHome.js
@@ -1,27 +1,31 @@
-import Categories from '../categories/Categories';
-import SideBar from '../sidebar/SideBar';
-import './userHome.css';
+import Categories from "../categories/Categories";
+import SideBar from "../sidebar/SideBar";
+import "./userHome.css";
 
-
-function UserHome(){
-    return(
-        <div className='home-container'>
-            <div>
-                <SideBar/>
-            </div>
-            <div className='main'>
-                <div className='main-hero' style={{ backgroundImage: "url(./images/hero-img.jpg)" }}>
-                    <div className='hero-text'>
-                        <h1>Welcome to <span className='app-name'>Handler</span></h1>
-                        <p>More than 50 services in your pocket</p>
-                    </div>
-                </div>
-                <div>
-                    <Categories/>
-                </div>
-            </div>
+function UserHome() {
+  return (
+    <div className="home-container">
+      <div>
+        <SideBar />
+      </div>
+      <div className="main">
+        <div
+          className="main-hero"
+          style={{ backgroundImage: "url(./images/hero-1.jpg)" }}
+        >
+          <div className="hero-text">
+            <h1>
+              Welcome to <span className="app-name">Handler</span>
+            </h1>
+            <p>More than 50 services in your pocket</p>
+          </div>
         </div>
-    );
+        <div>
+          <Categories />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default UserHome;
