28c99962e9c5f9621d27d04a89e106153e199c21
Faisal-Alsharif <ifaisal.alsharif@gmail.com>
2021-12-25 21:42:17 +0300

    changing Hero section and adding new image

--- services-platform-frontend/src/Components/userHome/UserHome.js
+++ services-platform-frontend/src/Components/userHome/UserHome.js
@@ -11,13 +11,16 @@ function UserHome() {
       <div className="main">
         <div
           className="main-hero"
-          style={{ backgroundImage: "url(./images/hero-1.jpg)" }}
+          // style={{ backgroundImage: "url(./images/hero-1.jpg)" }}
         >
           <div className="hero-text">
             <h1>
               Welcome to <span className="app-name">Handler</span>
             </h1>
             <p>More than 50 services in your pocket</p>
+          </div>
+          <div className="hero-img">
+            <img src="./images/hero-3.png" />
           </div>
         </div>
         <div>
